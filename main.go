af67816ce8ab64c7959bdc68004fe819ed5095be
Michael Trokel <michaelt96099@gmail.com>
2023-12-17 21:50:21 -0500

    Added "Display Route" function

--- main.go
+++ main.go
@@ -6,34 +6,101 @@ import (
 	"fmt"
 	"github.com/chromedp/cdproto/cdp"
 	"github.com/chromedp/chromedp"
-	"log"
-	"os"
-)
+		"log"
+		"os"
+	)
 
-type Flight struct {
-	Departure, Route, Destination, Altitude, Type, DepartureARTCC, DestinationARTCC string
-}
-
-var DepartureAirport, ArrivalAirport, AircraftRoute string
-var Number int
-
-func main() {
-	fmt.Println("Input Departure")
-	fmt.Scanln(&DepartureAirport)
-	fmt.Println("Input Arrival")
-	fmt.Scanln(&ArrivalAirport)
-	fmt.Println("Input Route")
-	scanner := bufio.NewScanner(os.Stdin)
-	if scanner.Scan() {
-		AircraftRoute = scanner.Text()
+	type Flight struct {
+		Departure, Route, Destination, Altitude, Type, DepartureARTCC, DestinationARTCC string
 	}
-	fmt.Println("Working...")
-	scraper()
-}
 
-func scraper() {
+	var DepartureAirport, ArrivalAirport, AircraftRoute string
+	var Number int
+	var checkOrDisplay string
+	func main() {
+		fmt.Println("Would you like to check(1) or display(2) routes")
+		fmt.Scanln(&checkOrDisplay)
+		if checkOrDisplay == "1" {
+			fmt.Println("Input Departure")
+		fmt.Scanln(&DepartureAirport)
+		fmt.Println("Input Arrival")
+		fmt.Scanln(&ArrivalAirport)
+		fmt.Println("Input Route")
+		scanner := bufio.NewScanner(os.Stdin)
+		if scanner.Scan() {
+			AircraftRoute = scanner.Text()
+		}
+		fmt.Println("Working...")
+		checkRoute()
+		} else if checkOrDisplay == "2" {
+			fmt.Println("Input Departure")
+		fmt.Scanln(&DepartureAirport)
+		fmt.Println("Input Arrival")
+		fmt.Scanln(&ArrivalAirport)
+		fmt.Println("Working...")
+		displayRoutes()
+		} else {
+			fmt.Println("Invalid Choice")
+			main()
+		}
+		
+	}
+
+	func displayRoutes() {
+		Link := fmt.Sprintf("https://nyartcc.org/prd?from=%s&to=%s", DepartureAirport, ArrivalAirport)
+		var correctRoutes []Flight
+
+		ctx, cancel := chromedp.NewContext(
+			context.Background(),
+		)
+		defer cancel()
+
+		var flightNodes []*cdp.Node
+		err := chromedp.Run(ctx,
+			chromedp.Navigate(Link),
+			chromedp.Nodes("tr.tier1", &flightNodes, chromedp.ByQueryAll),
+		)
+		if err != nil {
+			log.Fatal("Error:", err)
+		}
+
+		for _, node := range flightNodes {
+			var departure, route, destination, altitude, flightType, departureARTCC, destinationARTCC string
+
+			err := chromedp.Run(ctx,
+				chromedp.Text("td:nth-child(1)", &departure, chromedp.ByQuery, chromedp.FromNode(node)),
+				chromedp.Text("td:nth-child(2)", &route, chromedp.ByQuery, chromedp.FromNode(node)),
+				chromedp.Text("td:nth-child(3)", &destination, chromedp.ByQuery, chromedp.FromNode(node)),
+				chromedp.Text("td:nth-child(6)", &altitude, chromedp.ByQuery, chromedp.FromNode(node)),
+				chromedp.Text("td:nth-child(7)", &flightType, chromedp.ByQuery, chromedp.FromNode(node)),
+				chromedp.Text("td:nth-child(8)", &departureARTCC, chromedp.ByQuery, chromedp.FromNode(node)),
+				chromedp.Text("td:nth-child(9)", &destinationARTCC, chromedp.ByQuery, chromedp.FromNode(node)),
+			)
+
+			if err != nil {
+				log.Fatal("Error:", err)
+			}
+
+			flight := Flight{
+				Departure:        departure,
+				Route:            route,
+				Destination:      destination,
+				Altitude:         altitude,
+				Type:             flightType,
+				DepartureARTCC:   departureARTCC,
+				DestinationARTCC: destinationARTCC,
+			}
+			
+			correctRoutes = append(correctRoutes, flight)
+			
+	}
+	fmt.Println(correctRoutes)
+	main()
+	}
+
+
+func checkRoute() {
 	Link := fmt.Sprintf("https://nyartcc.org/prd?from=%s&to=%s", DepartureAirport, ArrivalAirport)
-
 	var correctRoutes []Flight
 
 	ctx, cancel := chromedp.NewContext(
